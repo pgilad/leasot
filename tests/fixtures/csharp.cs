c0baf5ea00a2cefdcb2c418da52f4603bb32b980
Mordechai Zuber <mez613@gmail.com>
2015-05-26 14:32:24 +0300

    add c# support

---
+++ tests/fixtures/csharp.cs
@@ -0,0 +1,13 @@
+// TODO: document file operations
+using System;
+using System.IO;
+
+public class Program
+{
+	public static void Main()
+	{
+		var lines = File.ReadAllLines("example.txt");
+		
+		/* FIXME do something with the file contents*/
+	}
+}
