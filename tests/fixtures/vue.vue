63caf5bb9b6f4b5197df9e1615ac6d77752222b3
Peter Altamirano <peter.altamirano@mediamonks.com>
2017-02-15 18:07:49 +0000

    Add support for Vue components

---
+++ tests/fixtures/vue.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- TODO: Vue template comment -->
+  <p>{{ greeting }} World!</p>
+</template>
+
+<script>
+/* TODO: Vue script comment */
+module.exports = {
+  data: function () {
+    return {
+      greeting: 'Hello Leasot!'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+p {
+  font-size: 2em;
+  // FIXME: Vue style comment
+  text-align: center;
+}
+</style>
