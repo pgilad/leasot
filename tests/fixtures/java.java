ab66af3b26719c7f4865acf2e84e213b593172be
Cyril Beslay <cicoub13@users.noreply.github.com>
2017-08-11 17:35:12 +0200

    Add java compatibility (#74)

---
+++ tests/fixtures/java.java
@@ -0,0 +1,12 @@
+import org.apache.package;
+
+public class HelloWorld
+{
+	public static void main(String[] args) {
+    // TODO Change language
+		System.out.println("Hello World!");
+    /*
+    * FIXME Log response
+    */
+	}
+}
