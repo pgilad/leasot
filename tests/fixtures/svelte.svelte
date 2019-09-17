eb43e6ccfa2c25d009668fbf6a8b3dc8cfdbde5b
John Obelenus <jobelenus@gmail.com>
2019-09-17 10:40:12 -0400

    Creating fixture for svelte

---
+++ tests/fixtures/svelte.svelte
@@ -0,0 +1,15 @@
+<script>
+/* TODO: Vue script comment */
+let greeting = 'Hello Leasot!'
+</script>
+
+<style lang="less" scoped>
+p {
+  font-size: 2em;
+  // FIXME: Vue style comment
+  text-align: center;
+}
+</style>
+
+<!-- TODO: Vue template comment -->
+<p>{{ greeting }} World!</p>
