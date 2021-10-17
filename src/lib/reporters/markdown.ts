ecc9c83c4ec254c43a34dab79833cf1d6cff6796
smc181002 <meherchaitanya18802@gmail.com>
2021-10-17 09:45:30 +0530

    closes #185 issue of missing characters at the end of table

--- src/lib/reporters/markdown.ts
+++ src/lib/reporters/markdown.ts
@@ -6,10 +6,10 @@ const reporterConfig: ReporterConfig = {
         if (ref) {
             text = `@${ref} ${text}`;
         }
-        return [`| [${file}](${file}#L${line}) | ${line} | ${text}`];
+        return [`| [${file}](${file}#L${line}) | ${line} | ${text} |`];
     },
     transformHeader(tag) {
-        return [`### ${tag}s`, `| Filename | line # | ${tag}`, '|:------|:------:|:------'];
+        return [`### ${tag}s`, `| Filename | line # | ${tag} |`, '|:------|:------:|:------|'];
     },
 };
 
